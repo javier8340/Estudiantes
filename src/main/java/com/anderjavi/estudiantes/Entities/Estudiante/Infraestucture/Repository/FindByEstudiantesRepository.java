4fb9a7cf5d6ffb2091bfea39a4f7e2ee77e4c949
Javier <jgg8340@gmail.com>
2021-05-04 15:46:08 +0200

    add repository

--- src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Infraestucture/Repository/FindByEstudiantesRepository.java
+++ src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Infraestucture/Repository/FindByEstudiantesRepository.java
@@ -3,12 +3,14 @@ package com.anderjavi.estudiantes.Entities.Estudiante.Infraestucture.Repository;
 import com.anderjavi.estudiantes.Entities.Estudiante.Domain.BusquedaCampos;
 import com.anderjavi.estudiantes.Entities.Estudiante.Domain.Estudiante;
 import com.anderjavi.estudiantes.Entities.Estudiante.Infraestucture.Repository.port.FindByEstudiantesPort;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class FindByEstudiantesRepository implements FindByEstudiantesPort {
     @Override
     public List<Estudiante> findBy(BusquedaCampos busquedaCampos) {
-        return null;
+        return null; //TODO implement
     }
 }
