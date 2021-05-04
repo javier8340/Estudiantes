cc332de9eee99910c8bcb6110117d9ffbba6c697
Javier <jgg8340@gmail.com>
2021-05-04 15:11:15 +0200

    fixes en repository

--- src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Infraestucture/Repository/CreateEstudianteRepository.java
+++ src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Infraestucture/Repository/CreateEstudianteRepository.java
@@ -2,6 +2,7 @@ package com.anderjavi.estudiantes.Entities.Estudiante.Infraestucture.Repository;
 
 import com.anderjavi.estudiantes.Entities.Estudiante.Domain.Estudiante;
 import com.anderjavi.estudiantes.Entities.Estudiante.Domain.EstudianteJpa;
+import com.anderjavi.estudiantes.Entities.Estudiante.Infraestucture.Repository.jpa.EstudianteRepositoryJpa;
 import com.anderjavi.estudiantes.Entities.Estudiante.Infraestucture.Repository.port.CreateEstudiantePort;
 import lombok.AllArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Repository
 public class CreateEstudianteRepository implements CreateEstudiantePort {
 
-    JpaRepository repository;
+    private final EstudianteRepositoryJpa repository;
 
     @Override
     public void create(Estudiante estudiante) throws Exception {
