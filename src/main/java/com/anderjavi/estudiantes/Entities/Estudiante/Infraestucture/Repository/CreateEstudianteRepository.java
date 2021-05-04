20cd46cbfac70d695d86e0b1c9331bc44783871c
Javier <jgg8340@gmail.com>
2021-05-04 12:55:30 +0200

    add method repository

--- src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Infraestucture/Repository/CreateEstudianteRepository.java
+++ src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Infraestucture/Repository/CreateEstudianteRepository.java
@@ -4,6 +4,7 @@ import com.anderjavi.estudiantes.Entities.Estudiante.Domain.Estudiante;
 import com.anderjavi.estudiantes.Entities.Estudiante.Infraestucture.Repository.port.CreateEstudiantePort;
 
 public class CreateEstudianteRepository implements CreateEstudiantePort {
+
     @Override
     public void create(Estudiante estudiante) throws Exception {
 
