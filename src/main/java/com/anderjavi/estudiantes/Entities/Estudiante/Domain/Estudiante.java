8ded0473e6f78204cf6c15d866510c755df8239a
Javier <jgg8340@gmail.com>
2021-05-04 13:06:17 +0200

    add Entity + Class Estudiante

--- src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Domain/Estudiante.java
+++ src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Domain/Estudiante.java
@@ -4,7 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @AllArgsConstructor @NoArgsConstructor @Data
 public class Estudiante {
+
+    Integer id;
+    String nombre;
+    String Apellido;
+    String correo;
+    Date fechaEntrada;
+    String ciudad;
+    int horasSemanales;
+    String especialidad;
+    String estado;
 
 }
--- src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Domain/EstudianteJpa.java
+++ src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Domain/EstudianteJpa.java
@@ -4,6 +4,25 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
 @AllArgsConstructor @NoArgsConstructor @Data
 public class EstudianteJpa {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer id;
+
+    String nombre;
+    String Apellido;
+    String correo;
+    Date fechaEntrada;
+    String ciudad;
+    int horasSemanales;
+    String especialidad;
+    String estado;
 }
