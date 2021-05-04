686c4a24596aa272962ce17c2b71e5456d8414ef
Javier <jgg8340@gmail.com>
2021-05-04 12:29:23 +0200

    create "create" Repository class

---
+++ src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Infraestucture/Repository/CreateEstudianteRepository.java
@@ -0,0 +1,11 @@
+package com.anderjavi.estudiantes.Entities.Estudiante.Infraestucture.Repository;
+
+import com.anderjavi.estudiantes.Entities.Estudiante.Domain.Estudiante;
+import com.anderjavi.estudiantes.Entities.Estudiante.Infraestucture.Repository.port.CreateEstudiantePort;
+
+public class CreateEstudianteRepository implements CreateEstudiantePort {
+    @Override
+    public void create(Estudiante estudiante) throws Exception {
+
+    }
+}
