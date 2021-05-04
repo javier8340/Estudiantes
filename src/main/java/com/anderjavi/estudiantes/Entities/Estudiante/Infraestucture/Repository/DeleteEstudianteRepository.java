fd099ea3f4b707394ec8be1a0e3a32b2ef5dca61
Javier <jgg8340@gmail.com>
2021-05-04 13:53:09 +0200

    fix typo on repository jpa

--- src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Infraestucture/Repository/DeleteEstudianteRepository.java
+++ src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Infraestucture/Repository/DeleteEstudianteRepository.java
@@ -1,18 +1,16 @@
 package com.anderjavi.estudiantes.Entities.Estudiante.Infraestucture.Repository;
 
-import com.anderjavi.estudiantes.Entities.Estudiante.Domain.Estudiante;
-import com.anderjavi.estudiantes.Entities.Estudiante.Domain.EstudianteJpa;
-import com.anderjavi.estudiantes.Entities.Estudiante.Infraestucture.Repository.jpa.EstudianteRepositorJpa;
+import com.anderjavi.estudiantes.Entities.Estudiante.Infraestucture.Repository.jpa.EstudianteRepositoryJpa;
 import com.anderjavi.estudiantes.Entities.Estudiante.Infraestucture.Repository.port.DeleteEstudiantePort;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class DeleteEstudianteRepository implements DeleteEstudiantePort {
 
     @Autowired
-    EstudianteRepositorJpa estudianteRepositorJpa;
+    EstudianteRepositoryJpa estudianteRepositoryJpa;
 
     @Override
     public void deleteById(int estudianteId) {
-        estudianteRepositorJpa.deleteById(estudianteId);
+        estudianteRepositoryJpa.deleteById(estudianteId);
     }
 }
