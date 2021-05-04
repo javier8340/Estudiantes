b96429b7f802698a7368ec7995bfde552070f093
Javier <jgg8340@gmail.com>
2021-05-04 12:55:41 +0200

    add tags to Estudiante

--- src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Domain/Estudiante.java
+++ src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Domain/Estudiante.java
@@ -1,4 +1,10 @@
 package com.anderjavi.estudiantes.Entities.Estudiante.Domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor @NoArgsConstructor @Data
 public class Estudiante {
+
 }
