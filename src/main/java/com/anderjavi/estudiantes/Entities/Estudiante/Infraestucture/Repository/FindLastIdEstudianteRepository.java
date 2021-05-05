68485edb397079339d043632d96c4d7ec0519b85
Javier <jgg8340@gmail.com>
2021-05-05 14:57:36 +0200

    default value

--- src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Infraestucture/Repository/FindLastIdEstudianteRepository.java
+++ src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Infraestucture/Repository/FindLastIdEstudianteRepository.java
@@ -1,11 +1,26 @@
 package com.anderjavi.estudiantes.Entities.Estudiante.Infraestucture.Repository;
 
 import com.anderjavi.estudiantes.Entities.Estudiante.Infraestucture.Repository.port.FindLastIdEstudiantePort;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@AllArgsConstructor
 public class FindLastIdEstudianteRepository implements FindLastIdEstudiantePort {
+
+    private JdbcTemplate jdbcTemplate;
+
+
+    @Autowired
+    public void DynamicRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
     public Integer getLastId() {
-        return null;
+
+        return 0;
     }
 }
