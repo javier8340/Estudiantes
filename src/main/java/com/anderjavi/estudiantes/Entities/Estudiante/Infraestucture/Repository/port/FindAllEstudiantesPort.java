8c5b2ba5f12a8e7e2be3eaa61ffb0e98445cf228
Javier <jgg8340@gmail.com>
2021-05-05 12:22:56 +0200

    find All changed to JBDC by ander

--- src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Infraestucture/Repository/port/FindAllEstudiantesPort.java
+++ src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Infraestucture/Repository/port/FindAllEstudiantesPort.java
@@ -1,9 +1,10 @@
 package com.anderjavi.estudiantes.Entities.Estudiante.Infraestucture.Repository.port;
 
+import com.anderjavi.estudiantes.Entities.Estudiante.Domain.Estudiante;
 import com.anderjavi.estudiantes.Entities.Estudiante.Domain.EstudianteJpa;
 
 import java.util.List;
 
 public interface FindAllEstudiantesPort {
-    List<EstudianteJpa> findAll();
+    List<Estudiante> findAll();
 }
