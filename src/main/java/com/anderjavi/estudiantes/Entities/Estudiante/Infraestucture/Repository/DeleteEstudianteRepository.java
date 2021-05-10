11acbda461bd897d9dbc7a44f5a00cd3e78fc3aa
Javier <jgg8340@gmail.com>
2021-05-10 09:51:44 +0200

    check deleteable to delete

--- src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Infraestucture/Repository/DeleteEstudianteRepository.java
+++ src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Infraestucture/Repository/DeleteEstudianteRepository.java
@@ -2,6 +2,8 @@ package com.anderjavi.estudiantes.Entities.Estudiante.Infraestucture.Repository;
 
 import com.anderjavi.estudiantes.Entities.Estudiante.Infraestucture.Repository.jpa.EstudianteRepositoryJpa;
 import com.anderjavi.estudiantes.Entities.Estudiante.Infraestucture.Repository.port.DeleteEstudiantePort;
+import com.anderjavi.estudiantes.Entities.Estudiante.Infraestucture.Repository.port.FindByIdEstudiantesPort;
+import com.anderjavi.estudiantes.exceptions.UnauthorizedException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
@@ -11,9 +13,21 @@ import org.springframework.stereotype.Repository;
 public class DeleteEstudianteRepository implements DeleteEstudiantePort {
 
     EstudianteRepositoryJpa estudianteRepositoryJpa;
+    FindByIdEstudiantesPort estudiantesPort;
 
     @Override
     public void deleteById(String estudianteId) {
+        checkDeleteable(estudianteId);
         estudianteRepositoryJpa.deleteById(estudianteId);
     }
+
+    private void checkDeleteable(String estudianteId){
+        try {
+            estudiantesPort.findById(estudianteId);
+        }catch (Exception e){
+            throw new UnauthorizedException();
+        }
+    }
+
+
 }
