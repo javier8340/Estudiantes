ec7e7f17d492feee2c9b19d59094aa37a576f741
Javier <jgg8340@gmail.com>
2021-05-11 12:25:59 +0200

    Add custom message to database exceptions

--- src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/application/exceptions/handler/EstudianteExceptionHandler.java
+++ src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/application/exceptions/handler/EstudianteExceptionHandler.java
@@ -3,6 +3,7 @@ package com.anderjavi.estudiantes.Entities.Estudiante.application.exceptions.han
 import com.anderjavi.estudiantes.Entities.Estudiante.application.exceptions.BranchException;
 import com.anderjavi.estudiantes.Entities.Estudiante.application.exceptions.CustomErrorResponse;
 import com.anderjavi.estudiantes.Entities.Estudiante.application.exceptions.EstudianteNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -33,6 +34,17 @@ public class EstudianteExceptionHandler extends ResponseEntityExceptionHandler {
         CustomErrorResponse errors = new CustomErrorResponse();
         errors.setTimestamp(LocalDateTime.now());
         errors.setError(ex.getMessage());
+        errors.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        return new ResponseEntity<>(errors, HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+    @ExceptionHandler({DataIntegrityViolationException.class })
+    public ResponseEntity<CustomErrorResponse> databaseExceptionHandler(DataIntegrityViolationException exception, WebRequest request) {
+
+        CustomErrorResponse errors = new CustomErrorResponse();
+        errors.setTimestamp(LocalDateTime.now());
+        errors.setError(exception.getMostSpecificCause().getMessage());
         errors.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
 
         return new ResponseEntity<>(errors, HttpStatus.INTERNAL_SERVER_ERROR);
