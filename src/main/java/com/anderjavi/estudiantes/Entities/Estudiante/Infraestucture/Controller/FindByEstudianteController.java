ddcf11e2e9029889d74e38fa4e26e065d45c2879
Ander Rodriguez <anderrodriguez205@gmail.com>
2021-05-11 12:35:25 +0200

    EstudianteNotFoundException update

--- src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Infraestucture/Controller/FindByEstudianteController.java
+++ src/main/java/com/anderjavi/estudiantes/Entities/Estudiante/Infraestucture/Controller/FindByEstudianteController.java
@@ -3,6 +3,7 @@ package com.anderjavi.estudiantes.Entities.Estudiante.Infraestucture.Controller;
 import com.anderjavi.estudiantes.Entities.Estudiante.Domain.dto.EstudianteSearchInputDto;
 import com.anderjavi.estudiantes.Entities.Estudiante.Domain.dto.EstudianteOutputDto;
 import com.anderjavi.estudiantes.Entities.Estudiante.Infraestucture.Repository.port.FindByEstudiantePort;
+import com.anderjavi.estudiantes.Entities.Estudiante.application.exceptions.EstudianteNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +16,10 @@ public class FindByEstudianteController {
     FindByEstudiantePort findByEstudiantePort;
 
     @PostMapping("/api/estudiante/findby")
-    public List<EstudianteOutputDto> findById(@RequestBody EstudianteSearchInputDto estudianteSearchInputDto) throws Exception {
+    public List<EstudianteOutputDto> findBy(@RequestBody EstudianteSearchInputDto estudianteSearchInputDto) {
+        if(findByEstudiantePort.findBy(estudianteSearchInputDto).isEmpty()) {
+            throw new EstudianteNotFoundException();
+        }
         return findByEstudiantePort.findBy(estudianteSearchInputDto);
     }
 }
